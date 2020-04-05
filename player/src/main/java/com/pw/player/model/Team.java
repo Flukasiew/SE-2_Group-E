10d2126109cbc0be2d8d8da0a18217d6741ba0b4
wizkiz <szy.majorek@gmail.com>
2020-04-05 19:27:46 +0200

    Team update

--- player/src/main/java/com/pw/player/model/Team.java
+++ player/src/main/java/com/pw/player/model/Team.java
@@ -1,4 +1,43 @@
 package com.pw.player.model;
 
 public class Team {
+    public TeamColor color;
+    public TeamRole role;
+    public int size;
+
+    public Team(TeamRole teamRole, TeamColor teamColor, int size) {
+        this.color = teamColor;
+        this.size = size;
+        this.role = teamRole;
+    }
+
+    enum TeamRole {
+        LEADER,
+        MEMBER
+    }
+
+    enum TeamColor {
+        RED,
+        BLUE
+    }
+
+    public TeamColor getColor() {
+        return color;
+    }
+
+    public void setColor(TeamColor teamColor) {
+        this.color = teamColor;
+    }
+
+    public TeamRole getRole() {
+        return role;
+    }
+
+    public void setRole(TeamRole teamRole) {
+        this.role = teamRole;
+    }
+
+    public boolean isLeader() {
+        return role == TeamRole.LEADER;
+    }
 }
