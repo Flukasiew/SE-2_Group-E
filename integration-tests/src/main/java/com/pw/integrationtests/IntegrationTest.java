a47408e61e7063d0cf92b1bf0e69545c7b90cf28
lukkaw <lukasz.kawka@syncron.com>
2020-05-17 11:08:04 +0200

    integration tests

--- integration-tests/src/main/java/com/pw/integrationtests/IntegrationTest.java
+++ integration-tests/src/main/java/com/pw/integrationtests/IntegrationTest.java
@@ -1,50 +1,90 @@
 package com.pw.integrationtests;
 
-import com.pw.gamemaster.GameMasterApp;
-import com.pw.player.PlayerApp;
-import com.pw.server.ServerApp;
-import org.json.simple.parser.ParseException;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.IntStream;
-
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pw.gamemaster.GameMasterApp;
+import com.pw.player.PlayerApp;
+import com.pw.server.ServerApp;
+
+import lombok.SneakyThrows;
+
 public class IntegrationTest {
 
-    @Test
-    public void shouldCompleteGame() throws InterruptedException {
-        int playersCount = 8;
+	private int playersCount = 0;
+	private Thread server;
+	private Thread gameMaster;
+	private List<Thread> players;
 
-        Thread server = new Thread(() -> ServerApp.main(null));
-        Thread gameMaster = new Thread(() -> {
-            try {
-                GameMasterApp.main(null);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        });
-        List<Thread> players = IntStream.range(0, playersCount)
-                .mapToObj(i -> new Thread(() -> PlayerApp.main(null)))
-                .collect(toList());
+	@Before
+	public void prepare() {
+		server = new Thread(() -> ServerApp.main(null));
+		gameMaster = new Thread(() -> {
+			try {
+				GameMasterApp.main(null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		players = IntStream.range(0, playersCount)
+				.mapToObj(i -> new Thread(() -> PlayerApp.main(null)))
+				.collect(toList());
+	}
 
-        server.start();
-        Thread.sleep(200);
-        gameMaster.start();
-        Thread.sleep(200);
-        players.forEach(Thread::start);
+	@Test
+	public void shouldCompleteGame() throws InterruptedException {
+		startThreads();
 
-        while(server.isAlive()) {
-            Thread.sleep(200);
-        }
-        Thread.sleep(200);
+		while (server.isAlive()) {
+			Thread.sleep(200);
+		}
+		Thread.sleep(200);
 
-        assertThat(gameMaster.isAlive()).isFalse();
-        players.forEach(player -> assertThat(player.isAlive()).isFalse());
-    }
+		assertThat(gameMaster.isAlive()).isFalse();
+		players.forEach(player -> assertThat(player.isAlive()).isFalse());
+	}
+
+	@Test
+	public void shouldEndGameWhenUnableReachGameMaster() throws InterruptedException {
+		startThreads();
+
+		Thread.sleep(2000);
+
+		gameMaster.stop();
+
+		Thread.sleep(200);
+
+		assertThat(server.isAlive()).isFalse();
+		players.forEach(player -> assertThat(player.isAlive()).isFalse());
+	}
+
+	@Test
+	public void shouldContinueGameWhenPlayerDisconnects() throws InterruptedException {
+		startThreads();
+
+		players.get(0).stop();
+
+		Thread.sleep(200);
+
+		assertThat(server.isAlive()).isTrue();
+		assertThat(gameMaster.isAlive()).isTrue();
+		for (int i = 1; i < players.size(); i++) {
+			assertThat(players.get(i).isAlive()).isTrue();
+		}
+	}
+
+	@SneakyThrows
+	private void startThreads() {
+		server.start();
+		Thread.sleep(200);
+		gameMaster.start();
+		Thread.sleep(200);
+		players.forEach(Thread::start);
+	}
 }
