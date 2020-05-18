11e321307bc2bf57ea45bbcf558ef19eae642038
wizkiz <szy.majorek@gmail.com>
2020-05-18 22:38:54 +0200

    discover returns status

--- game-master/src/main/java/com/pw/gamemaster/model/GameMaster.java
+++ game-master/src/main/java/com/pw/gamemaster/model/GameMaster.java
@@ -188,6 +188,7 @@ public class GameMaster {
                 }
             }
             GameMessageEndDTO gameMessageEndDTO;
+            ObjectMapper objectMapper = new ObjectMapper();
             if(blueWin) {
                 gameMessageEndDTO = new GameMessageEndDTO(Action.end, GameEndResult.BLUE);
             } else if (redWin) {
@@ -195,7 +196,7 @@ public class GameMaster {
             } else {
                 gameMessageEndDTO = new GameMessageEndDTO(Action.end, null);
             }
-            simpleClient.sendMessage(gameMessageEndDTO.toString());
+            simpleClient.sendMessage(objectMapper.writeValueAsString(gameMessageEndDTO));
         }
         catch (Exception e) {
             LOGGER.error("Exception occured when listening " + e.toString() +" from " + msg, e.toString());
@@ -478,8 +479,12 @@ public class GameMaster {
             case "discover":
                 List<Field> fieldList = board.discover(playersDTO.get(uuid).playerPosition);
                 ObjectMapper mapper = new ObjectMapper();
+                if(fieldList.size()==0){
+                    msg.put("status", "DENIED");
+                } else {
+                    msg.put("status", "OK");
+                }
                 msg.put("fields", mapper.writeValueAsString(fieldList));
-                // implement sending msg back
                 return msg;
 
             default:
